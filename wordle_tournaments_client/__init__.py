e11c2bb8bc9fa0e8107777ea7d7988cbba4673e3
Jeffrey Lym <jeffreyalym@gmail.com>
2022-01-19 14:00:36 -0600

    Add Tournament Runner

--- wordle_tournaments_client/__init__.py
+++ wordle_tournaments_client/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.0.1"
 from typing import Dict, List, Optional
 from dataclasses import dataclass
 import requests
-
+from abc import ABC, abstractmethod
 
 @dataclass(frozen=True)
 class User:
@@ -28,6 +28,8 @@ class Guess:
     num: int
     word: str
     score: str
+    letter_info: Dict[str, List[int]]
+    done: bool
 
 
 class Client:
@@ -101,5 +103,71 @@ class Client:
             game_id=json["game_id"], 
             num=1,
             word=json["word"],
-            score=json["score"]
+            score=json["score"],
+            letter_info=json["letter_info"],
+            done=json["done"]
         )
+
+class Solver(ABC):
+    @abstractmethod
+    def get_guess(
+        self, 
+        last_word_valid: bool,
+        last_word_score: str,
+        letter_info: Dict[str, List[int]]) -> str:
+        pass 
+
+class TournamentRunner:
+
+    solver: Solver
+    client: Client
+    user_id: int
+
+    def __init__(
+        self,
+        solver: Solver,
+        auth_code: str,
+        user_id: int,
+        server_url: Optional[str] = None ) -> None:
+
+        self.solver = solver
+        self.user_id = user_id
+        self.client = \
+            Client(auth_code, server_url) \
+            if server_url \
+            else Client(auth_code) 
+
+    def play_tournament(self) -> None:
+        for seed in (range(2000)):
+            game = self.client.create_game(self.user_id, seed)
+            self._play_game(game.game_id)
+
+    def _play_game(self, game_id: int) -> None:
+        num_turns = 0
+        done = False
+
+        last_word_valid = True
+        last_word_score = ""
+        letter_info: Dict[str, List[int]] = {}
+
+        while not done and num_turns < 100:
+            num_turns += 1
+            word = self.solver.get_guess(last_word_valid, last_word_score, letter_info)
+
+            try:
+                guess_result = self.client.create_guess(game_id, word)
+
+            except requests.HTTPError as err:
+                if err.response.status_code == 422:
+                    last_word_valid = False
+                    continue
+                raise
+
+            done = guess_result.done
+            last_word_score = guess_result.score
+            last_word_valid = True
+            letter_info = guess_result.letter_info
+
+
+
+            
