b53bc8abc48125ba1c77ab80475424b66c45a778
Jeffrey Lym <jeffreyalym@gmail.com>
2022-01-19 16:05:16 -0600

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='wordle_tournaments_client',
-    version='0.0.1',
+    version='0.0.2',
     author='',
     author_email='',
     description='Client for playing Wordle tournaments',
