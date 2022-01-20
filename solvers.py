1f8676b433dc68d82bd1ee7113de6ebb181dc5ec
Jeffrey Lym <jeffreyalym@gmail.com>
2022-01-19 20:21:16 -0600

    Write a solver that uses the frequency of characters to pick words

---
+++ solvers.py
@@ -0,0 +1,99 @@
+
+from collections import Counter
+from typing import List, Dict
+from wordle_tournaments_client import Solver, words
+
+
+def is_eligible(word: str, guess: str, score: str) -> bool:
+    word_char_freq = Counter(word)
+
+    # Process chars in the guess that are in the right position in the solution.
+    for i in range(5):
+        if score[i] != "g":
+            continue
+
+        guess_char = guess[i]       
+        word_char = word[i]
+        if guess_char != word_char:
+            return False
+
+        word_char_freq.subtract(word_char)
+
+    # Process chars in the guess that are in the solution, but are in the wrong position.
+    misplaced_chars = []
+    for i in range(5):
+        if score[i] != "y":
+            continue
+    
+        if guess[i] == word[i]:
+            return False
+
+        misplaced_chars.append(guess[i])
+
+    for misplaced_char in misplaced_chars:
+        if word_char_freq[misplaced_char] == 0:
+            return False
+        word_char_freq.subtract(misplaced_char)
+
+    # Process chars that are not in the solution.
+    for i in range(5):
+        if score[i] != "w":
+            continue
+        guess_char = guess[i]
+        if word_char_freq[guess_char] > 0:
+            return False
+
+    return True
+
+
+class CharFreqSolver(Solver):
+    eligible_words: List[str]
+    word_to_score: Dict[str, int]
+    
+    def __init__(self) -> None:
+        self.eligible_words = words.copy()
+
+
+    def pick_word(self) -> str:
+        char_counter: Counter[str] = Counter()
+        for word in self.eligible_words:
+            char_counter.update(word)
+
+        best_score = 0
+        best_word = ""
+
+        for word in self.eligible_words:
+            sum = 0
+            for c in word:
+                sum += char_counter[c]
+
+            if sum > best_score:
+                best_word = word
+                best_score = sum
+
+        return best_word
+
+    def filter_eligible_words(self, guess: str, score: str):
+        new_eligible_words: List[str] = []
+
+        for eligible_word in self.eligible_words:
+            if is_eligible(eligible_word, guess, score):
+                new_eligible_words.append(eligible_word)
+
+        self.eligible_words = new_eligible_words
+
+
+    def get_guess(
+        self,
+        last_word: str,
+        last_word_valid: bool,
+        last_word_score: str,
+        _: Dict[str, List[int]]) -> str:
+
+        if not last_word_valid:
+            raise ValueError("last word was not valid")
+
+        if last_word:
+            self.filter_eligible_words(last_word, last_word_score)
+
+        return self.pick_word()
---
+++ solvers_test.py
@@ -0,0 +1,27 @@
+from solvers import CharFreqSolver, is_eligible
+from wordle_tournaments_client import MemoryGameRunner
+
+def test_is_eligible():
+    assert is_eligible("apple", 'aeaea', 'gywww')
+
+    assert is_eligible("ccccc", "bbbbb", "wwwww")
+    assert not is_eligible("ccccb", "bbbbb", "wwwww")
+
+    assert is_eligible("aaaaa", "aaaaa", "ggggg")
+    assert not is_eligible("aaaab", "aaaaa", "ggggg")
+
+    assert is_eligible("baaac", "caaab", "ygggy")
+
+def test_char_freq_solver():
+    solver = CharFreqSolver()
+    runner = MemoryGameRunner("apple", solver, 10)
+    result = runner.play_game()
+    print(result)
+    assert result.won
+
+def test_char_freq_solver_2():
+    solver = CharFreqSolver()
+    runner = MemoryGameRunner("proxy", solver, 10)
+    result = runner.play_game()
+    print(result)
+    assert result.won
